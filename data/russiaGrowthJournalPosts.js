9aebf9b182bb223a354bb090e7ef4c22ab570309
amirmuratov007 <amirmuratov007@gmail.com>
2026-05-27 00:04:18 +0300

    Add files via upload

---
+++ data/russiaGrowthJournalPosts.js
@@ -0,0 +1,17 @@
+export const russiaGrowthJournalPosts = [
+  {
+    slug: 'kak-proverit-postavshchika-v-rossii',
+    title: 'Как проверить поставщика в России перед договором',
+    category: 'Поставщики'
+  },
+  {
+    slug: 'zakupochnyy-konflikt-interesov-red-flags',
+    title: 'Красные флаги закупочного конфликта интересов',
+    category: 'Закупки'
+  },
+  {
+    slug: 'proverka-direktora-kompanii',
+    title: 'Что показывает проверка директора компании',
+    category: 'Руководители'
+  }
+]
