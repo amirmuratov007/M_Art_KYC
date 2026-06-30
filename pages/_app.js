257ca1d005db6b77de61ac3d4ab8596cb7711f67
amuradov <amuradov@PC-WIN11-0286.corp.ntechlab.com>
2026-06-30 17:17:35 +0300

    Expose Yandex Metrika tag in document

--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,6 @@ const COOKIE_CONSENT_EVENT = 'heimdall-cookie-consent-change'
 
 export default function App({ Component, pageProps }) {
   const gaId = process.env.NEXT_PUBLIC_GA_ID
-  const ymId = process.env.NEXT_PUBLIC_YM_ID
   const [analyticsAllowed, setAnalyticsAllowed] = useState(false)
 
   useEffect(() => {
@@ -47,27 +46,6 @@ export default function App({ Component, pageProps }) {
             `}
           </Script>
         </>
-      )}
-
-      {analyticsAllowed && ymId && (
-        <Script id="yandex-metrika" strategy="afterInteractive">
-          {`
-            (function(m,e,t,r,i,k,a){
-              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-              m[i].l=1*new Date();
-              for (var j = 0; j < document.scripts.length; j++) {
-                if (document.scripts[j].src === r) { return; }
-              }
-              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-            })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-            ym(${ymId}, "init", {
-              clickmap:true,
-              trackLinks:true,
-              accurateTrackBounce:true,
-              webvisor:true
-            });
-          `}
-        </Script>
       )}
 
       <Script id="heimdall-service-worker" strategy="afterInteractive">
--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,8 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
+  const ymId = process.env.NEXT_PUBLIC_YM_ID
+
   return (
     <Html lang="ru">
       <Head>
@@ -20,8 +22,38 @@ export default function Document() {
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/site.webmanifest" />
+        {ymId && (
+          <script
+            id="yandex-metrika"
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(m,e,t,r,i,k,a){
+                  m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                  m[i].l=1*new Date();
+                  for (var j = 0; j < document.scripts.length; j++) {
+                    if (document.scripts[j].src === r) { return; }
+                  }
+                  k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+                })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+                ym(${ymId}, "init", {
+                  clickmap:true,
+                  trackLinks:true,
+                  accurateTrackBounce:true,
+                  webvisor:true
+                });
+              `
+            }}
+          />
+        )}
       </Head>
       <body>
+        {ymId && (
+          <noscript>
+            <div>
+              <img src={`https://mc.yandex.ru/watch/${ymId}`} style={{ position: 'absolute', left: '-9999px' }} alt="" />
+            </div>
+          </noscript>
+        )}
         <Main />
         <NextScript />
       </body>
